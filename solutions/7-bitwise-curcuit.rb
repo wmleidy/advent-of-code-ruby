bcdda26f373d676e45078c67d481f0f60331b148
Bill Leidy <wmleidy@gmail.com>
2015-12-19 01:37:25 -0600

    Brush up 7 and 8

--- solutions/7-bitwise-curcuit.rb
+++ solutions/7-bitwise-curcuit.rb
@@ -1,48 +1,5 @@
-# --- Day 7: Some Assembly Required ---
-
-# This year, Santa brought little Bobby Tables a set of wires and bitwise logic gates! Unfortunately, little Bobby is a little under the recommended age range, and he needs help assembling the circuit.
-
-# Each wire has an identifier (some lowercase letters) and can carry a 16-bit signal (a number from 0 to 65535). A signal is provided to each wire by a gate, another wire, or some specific value. Each wire can only get a signal from one source, but can provide its signal to multiple destinations. A gate provides no signal until all of its inputs have a signal.
-
-# The included instructions booklet describes how to connect the parts together: x AND y -> z means to connect wires x and y to an AND gate, and then connect its output to wire z.
-
-# For example:
-
-# 123 -> x means that the signal 123 is provided to wire x.
-# x AND y -> z means that the bitwise AND of wire x and wire y is provided to wire z.
-# p LSHIFT 2 -> q means that the value from wire p is left-shifted by 2 and then provided to wire q.
-# NOT e -> f means that the bitwise complement of the value from wire e is provided to wire f.
-# Other possible gates include OR (bitwise OR) and RSHIFT (right-shift). If, for some reason, you'd like to emulate the circuit instead, almost all programming languages (for example, C, JavaScript, or Python) provide operators for these gates.
-
-# For example, here is a simple circuit:
-
-# 123 -> x
-# 456 -> y
-# x AND y -> d
-# x OR y -> e
-# x LSHIFT 2 -> f
-# y RSHIFT 2 -> g
-# NOT x -> h
-# NOT y -> i
-# After it is run, these are the signals on the wires:
-
-# d: 72
-# e: 507
-# f: 492
-# g: 114
-# h: 65412
-# i: 65079
-# x: 123
-# y: 456
-# In little Bobby's kit's instructions booklet (provided as your puzzle input), what signal is ultimately provided to wire a?
-
-# --- Part Two ---
-
-# Now, take the signal you got on wire a, override wire b to that signal, and reset the other wires (including wire a). What new signal is ultimately provided to wire a?
-
-# For Part One, line #55 of 'help7-circuit.txt' read "19138 -> b"
+# For Part One, line #55 of 'input7-circuit.txt' reads "19138 -> b"
 # For Part Two, this needs to be changed to read "16076 -> b"
-
 
 class String
   def is_integer?
@@ -52,24 +9,19 @@ end
 
 def get_instructions
 	instructions_arr = []
-
-	IO.foreach("help7-circuit.txt") do |line|
+	IO.foreach("../input/input7-circuit.txt") do |line|
 		instructions_arr << line.chomp
 	end
-
 	instructions_arr
 end
 
-# in retrospect, probably would have been easier to sort just based on the last value in the string (!)
 def sort_instructions(arr)
-	arr.sort_by do |instruction|
-		variable_names = instruction.split.reject { |thing| thing == "->" || thing == "AND" || thing == "OR" || thing == "NOT" || thing == "LSHIFT" || thing == "RSHIFT" || thing.is_integer? }
-		max = variable_names.max
-		if max.length == 1 && !variable_names.select { |var| var.length == 2 }.empty?
-			max = variable_names.reject { |var| var.length == 1}.max
-		end
-		[max.length, max]
+	sorted = arr.sort_by do |instruction|
+		variable_to_set = instruction.split.last
+		[variable_to_set.length, variable_to_set]
 	end
+	# maneuver "a" to end of list
+	sorted.push(sorted.shift)
 end
 
 # Confirm that our instructions get ordered correctly...
@@ -80,8 +32,9 @@ def run_instructions(arr)
 		data = instr.split
 		var = data.last
 		instance_variable_set("@#{var}", parser(data[0..-3]))
-		puts "#{var}:" + instance_variable_get("@#{var}").to_s
+		# puts "#{var}:" + instance_variable_get("@#{var}").to_s
 	end
+	# the answer...
 	@a
 end
 
@@ -97,8 +50,10 @@ def parser(arr)
 	# cases involving NOT
 	elsif arr.length == 2
 		65535 - instance_variable_get("@#{arr[1]}")
+	# all other operations
 	else
-		case arr[1]
+		operator = arr[1]
+		case operator
 		when "AND"
 			first  = arr[0].is_integer? ? arr[0].to_i : instance_variable_get("@#{arr[0]}")
 			second = arr[2].is_integer? ? arr[2].to_i : instance_variable_get("@#{arr[2]}")
@@ -117,57 +72,9 @@ end
 
 p run_instructions(sort_instructions(get_instructions))
 
-### brilliant Ruby solution ###
-# trans = {
-#   'AND'    => '&',
-#   'OR'     => '|',
-#   'NOT'    => '~',
-#   'LSHIFT' => '<<',
-#   'RSHIFT' => '>>'
-# }
+### Other, neater and cleaner approaches (from Reddit) ###
 
-# p eval ARGF.
-#   read.
-#   gsub(Regexp.union(trans.keys), trans).
-#   gsub(/(.+?) -> (\w+)/) { "%2s = #$1" % $2 }.
-#   upcase.
-#   split("\n").
-#   sort.
-#   rotate.
-#   join(?;)
-
-### class-based Ruby solution ###
-# class Circuit
-#   TRANSFORMS = {
-#     "LSHIFT"         => "<<",
-#     "RSHIFT"         => ">>",
-#     "NOT"            => "~",
-#     "AND"            => "&",
-#     "OR"             => "|",
-#     /\b(if|do|in)\b/ => "\\1_",
-#   }
-
-#   def add(line)
-#     TRANSFORMS.each do |from, to|
-#       line.gsub!(from, to)
-#     end
-
-#     expr, name = line.strip.split(" -> ")
-
-#     method = "def #{name}; @#{name} ||= #{expr}; end"
-
-#     puts method
-#     instance_eval method
-#   end
-# end
-
-# circuit = Circuit.new
-# open("input.txt").each_line { |line| circuit.add(line) }
-# # circuit.add("46065 -> b")
-# p circuit.a
-
-### a third Ruby solution ###
-
+### 1 ### 
 # input = DATA.read.chomp.split("\n")
 # substitutions = {'AND' => '&', 'OR' => '|', 'NOT' => '~', 
 #                  'RSHIFT' => '>>', 'LSHIFT' => '<<'}
@@ -186,3 +93,22 @@ p run_instructions(sort_instructions(get_instructions))
 # end
 
 # p @a_
+
+### 2 ###
+# trans = {
+#   'AND'    => '&',
+#   'OR'     => '|',
+#   'NOT'    => '~',
+#   'LSHIFT' => '<<',
+#   'RSHIFT' => '>>'
+# }
+
+# p eval ARGF.
+#   read.
+#   gsub(Regexp.union(trans.keys), trans).
+#   gsub(/(.+?) -> (\w+)/) { "%2s = #$1" % $2 }.
+#   upcase.
+#   split("\n").
+#   sort.
+#   rotate.
+#   join(?;)
