ecdc660e95728a5587f2788bcf1a467cd6d99e71
Bill Leidy <wmleidy@gmail.com>
2015-12-18 19:15:59 -0600

    Refactor first six solutions

--- solutions/6-xmas-lights.rb
+++ solutions/6-xmas-lights.rb
@@ -1,130 +1,63 @@
-# --- Day 6: Probably a Fire Hazard ---
-
-# Because your neighbors keep defeating you in the holiday house decorating contest year after year, you've decided to deploy one million lights in a 1000x1000 grid.
-
-# Furthermore, because you've been especially nice this year, Santa has mailed you instructions on how to display the ideal lighting configuration.
-
-# Lights in your grid are numbered from 0 to 999 in each direction; the lights at each corner are at 0,0, 0,999, 999,999, and 999,0. The instructions include whether to turn on, turn off, or toggle various inclusive ranges given as coordinate pairs. Each coordinate pair represents opposite corners of a rectangle, inclusive; a coordinate pair like 0,0 through 2,2 therefore refers to 9 lights in a 3x3 square. The lights all start turned off.
-
-# To defeat your neighbors this year, all you have to do is set up your lights by doing the instructions Santa sent you in order.
-
-# For example:
-
-# turn on 0,0 through 999,999 would turn on (or leave on) every light.
-# toggle 0,0 through 999,0 would toggle the first line of 1000 lights, turning off the ones that were on, and turning on the ones that were off.
-# turn off 499,499 through 500,500 would turn off (or leave off) the middle four lights.
-# After following the instructions, how many lights are lit?
-
-# --- Part Two ---
-
-# You just finish implementing your winning light pattern when you realize you mistranslated Santa's message from Ancient Nordic Elvish.
-
-# The light grid you bought actually has individual brightness controls; each light can have a brightness of zero or more. The lights all start at zero.
-
-# The phrase turn on actually means that you should increase the brightness of those lights by 1.
-
-# The phrase turn off actually means that you should decrease the brightness of those lights by 1, to a minimum of zero.
-
-# The phrase toggle actually means that you should increase the brightness of those lights by 2.
-
-# What is the total brightness of all lights combined after following Santa's instructions?
-
-# For example:
-
-# turn on 0,0 through 0,0 would increase the total brightness by 1.
-# toggle 0,0 through 999,999 would increase the total brightness by 2000000.
-
-
 def create_grid
-	# Array.new(1000) { Array.new(1000, false) }  # Part One
-	Array.new(1000) { Array.new(1000, 0) }
+  # Array.new(1000) { Array.new(1000, false) } # Part One
+  Array.new(1000) { Array.new(1000, 0) }     # Part Two
 end
 
-def count_lights_on(matrix)
-	count = 0
-	matrix.each do |row|
-		row.each do |cell|
-			# count += 1 if cell  # Part One
-			count += cell
-		end
-	end
-	count
+def count_lights(matrix)
+  matrix.inject(0) do |matrix_total, row|
+    matrix_total += row.inject(0) do |row_total, cell|
+      # cell ? row_total += 1 : row_total # Part One
+      row_total += cell                 # Part Two
+    end
+  end
 end
 
 def process_instructions
-	grid = create_grid
+  grid = create_grid
 
-	IO.foreach("help6-light-instructions.txt") do |line|
-		instr = line.split
-		if instr[1] == "on"
-			# grid = turn_one_way(grid, instr[2], instr[4], true)  # Part One
-			grid = turn_one_way(grid, instr[2], instr[4], 1)
-		elsif instr[1] == "off"
-			# grid = turn_one_way(grid, instr[2], instr[4], false)  # Part One
-			grid = turn_one_way(grid, instr[2], instr[4], -1)
-		elsif instr[0] == "toggle"
-			grid = toggle(grid, instr[1], instr[3])
-		end
-	end
-	count_lights_on(grid)
+  IO.foreach("../input/input6-light-instructions.txt") do |line|
+    instr = line.gsub(/turn\s/, "").gsub(/through\s/, "").split
+    method = instr[0]
+    start_values = instr[1]
+    end_values = instr[2]
+    start_x = start_values.split(",")[0].to_i
+    start_y = start_values.split(",")[1].to_i
+    end_x   = end_values.split(",")[0].to_i
+    end_y   = end_values.split(",")[1].to_i
+
+    if method == "on"
+      # grid = turn_one_way(grid, start_x, end_x, start_y, end_y, true) # Part One
+      grid = turn_one_way(grid, start_x, end_x, start_y, end_y, 1)    # Part Two
+    elsif method == "off"
+      # grid = turn_one_way(grid, start_x, end_x, start_y, end_y, false) # Part One
+      grid = turn_one_way(grid, start_x, end_x, start_y, end_y, -1)    # Part Two
+    elsif method == "toggle"
+      grid = toggle(grid, start_x, end_x, start_y, end_y)
+    end
+  end
+  count_lights(grid)
 end
 
-def turn_one_way(matrix, start_values, end_values, value)
-	start_x = start_values.split(",")[0].to_i
-	start_y = start_values.split(",")[1].to_i
-	end_x   = end_values.split(",")[0].to_i
-	end_y   = end_values.split(",")[1].to_i
-
-	i = start_x
-	while i <= end_x
-		j = start_y
-		while j <= end_y
-			# matrix[i][j] = boolean  # Part One
-			matrix [i][j] += value
-			matrix [i][j] = 0 if matrix[i][j] < 0
-			j += 1
-		end
-		i += 1
-	end
-	matrix
+def turn_one_way(matrix, start_x, end_x, start_y, end_y, value)
+  for i in (start_x..end_x)
+    for j in (start_y..end_y)
+      # matrix[i][j] = value                  # Part One
+      matrix [i][j] += value                # Part Two
+      matrix [i][j] = 0 if matrix[i][j] < 0 # Part Two
+    end
+  end
+  matrix
 end
 
-def toggle(matrix, start_values, end_values)
-	start_x = start_values.split(",")[0].to_i
-	start_y = start_values.split(",")[1].to_i
-	end_x   = end_values.split(",")[0].to_i
-	end_y   = end_values.split(",")[1].to_i
-
-	i = start_x
-	while i <= end_x
-		j = start_y
-		while j <= end_y
-			# matrix[i][j] ? matrix[i][j] = false : matrix[i][j] = true  # Part One
-			matrix[i][j] += 2
-			j += 1
-		end
-		i += 1
-	end
-	matrix
+def toggle(matrix, start_x, end_x, start_y, end_y)
+  for i in (start_x..end_x)
+    for j in (start_y..end_y)
+      # matrix[i][j] ? matrix[i][j] = false : matrix[i][j] = true # Part One
+      matrix[i][j] += 2                                         # Part Two
+    end
+  end
+  matrix
 end
 
 p process_instructions
-
-
-### Reddit answer for Part Two ### 
-
-# require 'matrix'
-
-# controls = { 'turn on' => 1, 'turn off' => -1, 'toggle' => 2 }
-
-# grid = Matrix.zero(1000)
-# IO.readlines('input.txt').each { |str|
-#   str =~ /\A(.*)\s(\d+),(\d+)\sthrough\s(\d+),(\d+)/
-#   ($2..$4).each { |x|
-#     ($3..$5).each { |y|
-#       grid.send(:[]=, x.to_i, y.to_i, [0, grid[x.to_i, y.to_i] + controls[$1]].max)
-#     }
-#   }
-# }
-
-# puts grid.reduce(:+)
+# To run for other Part, comment/uncomment lines above
