fff30488f97ee45f0ec7025fcb7bea4e30451048
Bill Leidy <wmleidy@gmail.com>
2016-01-15 20:00:31 -0600

    Modify Day 10 solution to make faster

--- solutions/10-elves-look-and-say.rb
+++ solutions/10-elves-look-and-say.rb
@@ -1,9 +1,7 @@
-# My EXTREMELY SLOW solution, so slow that it's actually worthless
-# (Storing the strings in memory gets ridiculously costly very quickly)
+# My SLOW solution (made considerably faster by using << instead of + to combined strings)
 def look_and_say_wrapper(input, iterations = 40)
   iterations.times do |x|
     input = look_and_say(input)
-    # p x
   end
   input.to_s.length
 end
@@ -18,8 +16,8 @@ def look_and_say(input)
 
   while i < old_str.length
     if prev != "" && prev != old_str[i]
-      new_str += count.to_s
-      new_str += prev
+      new_str << count.to_s
+      new_str << prev
       count = 1
     else
       count += 1
@@ -27,18 +25,18 @@ def look_and_say(input)
     prev = old_str[i]
     i += 1
   end
-  new_str += count.to_s
-  new_str += prev
+  new_str << count.to_s
+  new_str << prev
   new_str.to_i
 end
 
-# p look_and_say_wrapper(1321131112)
-# p look_and_say_wrapper(1321131112, 50)
+p look_and_say_wrapper(1321131112)
+p look_and_say_wrapper(1321131112, 50)
 
 ### Someone else's very fast and very good solution ###
-input = '1321131112'
+# input = '1321131112'
 
-50.times do |count|
-  input = input.gsub(/(.)\1*/) { |s| s.size.to_s + s[0] }
-  puts "#{count + 1}: #{input.length}"
-end
+# 50.times do |count|
+#   input = input.gsub(/(.)\1*/) { |s| s.size.to_s + s[0] }
+#   puts "#{count + 1}: #{input.length}"
+# end
