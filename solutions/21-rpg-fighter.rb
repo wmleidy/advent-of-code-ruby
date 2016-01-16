b5332aae6db73fb664ec319a0e1e28cd34f5b37a
Bill Leidy <wmleidy@gmail.com>
2016-01-15 22:55:54 -0600

    Refactor Day 21 answer to match blog post

--- solutions/21-rpg-fighter.rb
+++ solutions/21-rpg-fighter.rb
@@ -7,35 +7,26 @@ class Item
     @damage = damage
     @defense = defense
   end
-
 end
 
 class Weapon < Item
-  def initialize(name, cost, damage, defense)
-    super
-  end
 end
 
 class Armor < Item
-  def initialize(name, cost, damage, defense)
-    super
-  end
 end
 
 class Ring < Item
-  def initialize(name, cost, damage, defense)
-    super
-  end
 end
 
 class Character
-  attr_accessor :name, :hit_points, :damage, :defense, :equipment
+  attr_reader :name, :equipment, :damage, :defense
+  attr_accessor :hit_points
 
   def initialize(name, hit_points, options = {})
     @name = name
     @hit_points = hit_points
     @equipment = options[:equipment] || []
-    @damage = options[:damage] || calculate_attack
+    @damage  = options[:damage]  || calculate_attack
     @defense = options[:defense] || calculate_absorb
   end
 
@@ -45,12 +36,11 @@ class Character
     target.hit_points -= result
   end
 
-  def duel_to_the_death!(enemy, hero = self)
-    antagonists = [hero, enemy]
-    antagonists.cycle do |character|
-      # for the hero, target is equal to arr[-1], i.e. the enemy
-      # for the boss, target is equal to arr[0] , i.e. the hero
-      target = antagonists[antagonists.index(character) - 1]
+  def duel_to_the_death(enemy)
+    hero = self
+    combatants = [hero, enemy]
+    combatants.cycle do |character|
+      character == hero ? target = enemy : target = hero
       return character if character.attack(target) <= 0
     end
   end
@@ -58,14 +48,13 @@ class Character
   private
 
     def calculate_attack
-      equipment.inject(0) { |total, thing| total += thing.damage }
+      equipment.inject(0) { |total, item| total += item.damage }
     end
 
     def calculate_absorb
-      equipment.inject(0) { |total, thing| total += thing.defense }
+      equipment.inject(0) { |total, item| total += item.defense }
     end
 end
-
 
 weapons = [
   Weapon.new("Dagger", 8, 4, 0),
@@ -105,8 +94,8 @@ def find_cheapest_win_and_most_expensive_loss(weapons, armor, rings)
       rings.combination(2).to_a.each do |r|
         arms = [w, a, r].flatten
         hero = Character.new("Knowledge", 100, { equipment: arms })
-        villain = Character.new("Ignorance", 103, { damage: 9, defense: 2})
-        if hero.duel_to_the_death!(villain) == hero
+        boss = Character.new("Ignorance", 103, { damage: 9, defense: 2})
+        if hero.duel_to_the_death(boss) == hero
           winning_sets << arms
         else
           losing_sets << arms
@@ -115,12 +104,13 @@ def find_cheapest_win_and_most_expensive_loss(weapons, armor, rings)
     end
   end
 
-  cheapest = winning_sets.min_by { |set| set.inject(0) { |total, item| total += item.cost } }
-  cheapest_cost = cheapest.inject(0) { |total, item| total += item.cost }
+  cheapest_set  = winning_sets.min_by { |set| set.inject(0) { |total, item| total += item.cost } }
+  cheapest_cost = cheapest_set.inject(0) { |total, item| total += item.cost }
+  puts "Cheapest win: #{cheapest_cost} GP with #{cheapest_set.map(&:name).join(", ")}"
 
-  costliest = losing_sets.max_by { |set| set.inject(0) { |total, item| total += item.cost } }
-  costliest_cost = costliest.inject(0) { |total, item| total += item.cost }
-  puts "Cheapest win: #{cheapest_cost}, Costliest loss: #{costliest_cost}"
+  costliest_set  = losing_sets.max_by { |set| set.inject(0) { |total, item| total += item.cost } }
+  costliest_cost = costliest_set.inject(0) { |total, item| total += item.cost }
+  puts "Costliest loss: #{costliest_cost} GP with #{costliest_set.map(&:name).join(", ")}"
 end
 
 find_cheapest_win_and_most_expensive_loss(weapons, armor, rings)
